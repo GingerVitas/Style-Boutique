12b6f8cca69aed45ce18fff9b25eac7cb048a671
janie-lee-developer <janie.lee.inquire@gmail.com>
2022-04-16 19:06:59 -0400

    product datatype removed ENUM and then replaced to string, to prevent creating duplicated colors.

--- script/seed.js
+++ script/seed.js
@@ -1,6 +1,7 @@
 'use strict'
 
-const {db, models: {User, Product} } = require('../server/db')
+const {db, models: {User, Product} } = require('../server/db');
+const { faker } = require("@faker-js/faker");
 
 /**
  * seed - this function clears the database, updates tables to
@@ -10,13 +11,57 @@ async function seed() {
   await db.sync({ force: true }) // clears db and matches models to tables
   console.log('db synced!')
 
-  // Creating Users
-  const users = await Promise.all([
-    User.create({ username: 'cody', password: '123', firstName: 'Cody', lastName:'Harris', email: 'codyH@gmail.com'}),
-    User.create({ username: 'murphy', password: '123', firstName: 'Murphy', lastName: 'Kim', email: 'murphyK@gmail.com' }),
-  ])
+  // Creating Users ****************************************************************
+  await Promise.all(
+    Array(5).fill().map(user => {
+      const firstName = faker.name.firstName();
+      const lastName = faker.name.lastName();
+      return User.create({
+        username: `${firstName.toLowerCase()}_${lastName.toLowerCase()}`,
+        password: '123',
+        firstName,
+        lastName,
+        email: `${firstName.toLowerCase()}.${lastName.toLowerCase()}@gmail.com`
+      })
+    })
+  )
 
+  const users = await Promise.all([
+    User.create({ username: 'cody', password: '123', firstName: 'Cody', lastName:'Harris', email: 'cody_harris@gmail.com'}),
+    User.create({ username: 'murphy', password: '123', firstName: 'Murphy', lastName: 'Kim', email: 'dummy_email@gmail.com' }),
+  ]);
+  
   console.log(`seeded ${users.length} users`);
+
+  // Creating Products ****************************************************************
+
+  const colors = ["white", "black", "red", "blue", "gray", "orange", "green"];
+  const sizes = ["XS", "S", "M", "L", "XL"];
+
+  let usedColors = [];
+
+  const generateColor = ()=>{
+    for (let i = 0; i < colors.length; i++){
+      if(!usedColors.includes(colors[i])){
+        usedColors.push(colors[i]);
+        return colors[i]
+      }
+    }
+  }
+
+  const dresses = await Promise.all(
+    Array(colors.length).fill().map(product => {
+      return Product.create({
+        category: 'dresses',
+        name: `${faker.word.adjective(1)} dress`,
+        imageUrl: faker.image.fashion(),
+        color: generateColor(),
+        size: 'S',
+        availableStock: 5,
+        price: 20.50
+      })
+    })
+  );
 
   console.log(`seeded successfully`)
   return {
