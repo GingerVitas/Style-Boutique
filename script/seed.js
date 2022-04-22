4d6bf50b9723cff89a5fe588e078e872b2387b13
Scott Johnson <singinbaritone@gmail.com>
2022-04-21 17:29:19 -0700

    added ProductSKU and Category models, changed seed method to fit database

--- script/seed.js
+++ script/seed.js
@@ -1,7 +1,29 @@
 'use strict'
-const {  dresses, tops, jeans, leggings, pants, sweaters, skirts, shoes, belts, accessories} = require('./seed-data')
-const {db, models: {User, Product} } = require('../server/db');
+const {  dressesProduct, dressData /*tops, jeans, leggings, pants, sweaters, skirts, shoes, belts, accessories */} = require('./seed-data')
+const {db, models: {User, Product, ProductSKU, Category} } = require('../server/db');
 const { faker } = require("@faker-js/faker");
+
+const categories = [
+  {categoryName: 'Top'},
+  {categoryName: 'Skirt'},
+  {categoryName: 'Dress'},
+  {categoryName: 'Jeans'},
+  {categoryName: 'Leggings'},
+  {categoryName: 'Pants'},
+  {categoryName: 'Sweater'},
+  {categoryName: 'Shoes'},
+  {categoryName: 'Belts'},
+  {categoryName: 'Accessories'}
+];
+
+const productFinder = async (productName) => {
+  const product =  await db.models.product.findOne({where: {name:productName}})
+  return product
+}
+const categoryFinder = async (category) => {
+  const foundCategory =  await db.models.category.findOne({where: {categoryName:category}})
+  return foundCategory
+}
 
 /**
  * seed - this function clears the database, updates tables to
@@ -35,16 +57,37 @@ async function seed() {
 
   // Creating Products ****************************************************************
 
+  await Category.bulkCreate(categories).then(console.log(`****${categories.length} Categories Seeded****`))
+  let dresses = await Promise.all(dressesProduct.flatMap(async(dress)=> {
+    const foundCategory = await categoryFinder('Dress');
+    return [
+      {...dress, categoryId: foundCategory.id}
+    ]
+  }));
+  dresses = dresses.flat();
   await Product.bulkCreate(dresses).then(console.log(`**** ${dresses.length} Dresses Seeded****`));
-  await Product.bulkCreate(tops).then(console.log(`**** ${tops.length} Tops Seeded****`))
-  await Product.bulkCreate(jeans).then(console.log(`**** ${jeans.length} Jeans Seeded****`));
-  await Product.bulkCreate(pants).then(console.log(`**** ${pants.length} Pants Seeded****`));
-  await Product.bulkCreate(sweaters).then(console.log(`**** ${sweaters.length} Sweaters Seeded****`));
-  await Product.bulkCreate(skirts).then(console.log(`**** ${skirts.length} Skirts Seeded****`));
-  await Product.bulkCreate(leggings).then(console.log(`****${leggings.length} Leggings Seeded****`));
-  await Product.bulkCreate(shoes).then(console.log(`**** ${shoes.length} Shoes Seeded****`));
-  await Product.bulkCreate(belts).then(console.log(`**** ${belts.length} Belts Seeded****`));
-  await Product.bulkCreate(accessories).then(console.log(`**** ${accessories.length} Accessories Seeded****`));
+  let dressSKUs = await Promise.all(dressData.flatMap(async(dress)=>{
+    const product = await productFinder(dress.productName);
+    const foundCategory = await categoryFinder('Dress');
+    return [
+     {...dress, availableStock: Math.floor(Math.random()*15), size: "XS", productId: product.id, categoryId: foundCategory.id},
+     {...dress, availableStock: Math.floor(Math.random()*15), size: "Small", productId: product.id, categoryId: foundCategory.id},
+     {...dress, availableStock: Math.floor(Math.random()*15), size: "Medium", productId: product.id, categoryId: foundCategory.id},
+     {...dress, availableStock: Math.floor(Math.random()*15), size: "Large", productId: product.id, categoryId: foundCategory.id},
+     {...dress, availableStock: Math.floor(Math.random()*15), size: "XL", productId: product.id, categoryId: foundCategory.id},
+    ]
+  }));
+  dressSKUs = dressSKUs.flat();
+  await ProductSKU.bulkCreate(dressSKUs).then(console.log(`**** ${dressSKUs.length} Dress SKUs Seeded****`));
+  // await Product.bulkCreate(tops).then(console.log(`**** ${tops.length} Tops Seeded****`))
+  // await Product.bulkCreate(jeans).then(console.log(`**** ${jeans.length} Jeans Seeded****`));
+  // await Product.bulkCreate(pants).then(console.log(`**** ${pants.length} Pants Seeded****`));
+  // await Product.bulkCreate(sweaters).then(console.log(`**** ${sweaters.length} Sweaters Seeded****`));
+  // await Product.bulkCreate(skirts).then(console.log(`**** ${skirts.length} Skirts Seeded****`));
+  // await Product.bulkCreate(leggings).then(console.log(`****${leggings.length} Leggings Seeded****`));
+  // await Product.bulkCreate(shoes).then(console.log(`**** ${shoes.length} Shoes Seeded****`));
+  // await Product.bulkCreate(belts).then(console.log(`**** ${belts.length} Belts Seeded****`));
+  // await Product.bulkCreate(accessories).then(console.log(`**** ${accessories.length} Accessories Seeded****`));
 
 
 
