1310cff0e9009bf6107b500412688897c42b3c50
janie-lee-developer <janie.lee.inquire@gmail.com>
2022-05-02 23:33:31 -0400

    product detail styling

--- client/components/pages/SingleProduct.js
+++ client/components/pages/SingleProduct.js
@@ -5,9 +5,10 @@ import {useParams} from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
 import { addToCart } from '../../store/cart';
 import {loadColors} from '../../store/productColors';
+import { loadSKUs } from '../../store/skus';
 
 //MUI
-import { Button, InputLabel, MenuItem, FormControl, Select } from '@mui/material';
+import { Button, InputLabel, MenuItem, FormControl, Select, Grid } from '@mui/material';
 
 //Child Component
 // import ProductCard from '../ProductCard';
@@ -17,6 +18,7 @@ const singleProduct = () => {
   const {productName} = useParams();
   const product = useSelector(state => (state.products.find(product => product.name === productName)));
   const colors = useSelector(state=>state.productColors);
+  const sku = useSelector(state => state.skus);
   const order = useSelector( state => state.order );
   const [size, setSize] = useState('');
   const [color, setColor] = useState('');
@@ -31,13 +33,11 @@ const singleProduct = () => {
   })
 
   useEffect(()=> {
-    dispatch(loadColors(productName))
+    dispatch(loadColors(productName)),
+    dispatch(loadSKUs(productName))
   }, [])
 
-  if (!product || !colors.length) return (
-    <div>Loading...</div>
-  )
-  console.log('ORDER', order)
+  console.log('SKUS', sku)
   // For Size DropDown
   const handleSize = (event) => {
     const selectedSKU = event.target.value;
@@ -52,55 +52,62 @@ const singleProduct = () => {
     setLineItem({ ...lineItem, productName: product.name, productColor: selectedColor.color, imageUrl: selectedColor.imageUrl});
   };
 
+  if (!product || !colors.length) return (
+    <div>Loading...</div>
+  )
   return (
     <div>
-      <table>
-        <tbody>
-          <tr>
-            <th>Product Brand</th>
-            <th>Product Name</th>
-            <th>Price</th>
-          </tr>
-          <tr>
-            <td>{product.brand}</td>
-            <td>{product.name}</td>
-          </tr>
-        </tbody>
-      </table>
-      <FormControl sx={{ width: '50%' }}>
-        <InputLabel id="demo-simple-select-label">Size</InputLabel>
-        <Select
-          labelId="demo-simple-select-label"
-          id="demo-simple-select"
-          value={size}
-          label="Size"
-          onChange={(e)=> handleSize(e)}
-        >
-          { !color ? <MenuItem value={''}>Please Select a Color</MenuItem> :
-            color.productSKUs.map(sku => sku.availableStock === 0 ? 
-            <MenuItem key={sku.id} value={sku} disabled={true} >{sku.size}  Out of Stock, check back soon!</MenuItem> 
-            : <MenuItem key={sku.id} value={sku}>{sku.size}  {sku.availableStock <= 3 ? `Only ${sku.availableStock} left!` : `${sku.availableStock}`}</MenuItem>)
-          }
-        </Select>
-      </FormControl>
-      <br />
-      <FormControl sx={{ width: '50%' }}>
-        <InputLabel id="demo-simple-select-label">Color</InputLabel>
-        <Select
-          labelId="demo-simple-select-label"
-          id="demo-simple-select"
-          value={color}
-          label="Color"
-          onChange={handleColor}
-          defaultValue={0}
-        >
-          {
-            colors.map(color => <MenuItem key={color.id} value={color}>{color.color}</MenuItem>)
-          }
-        </Select>
-      </FormControl>
-      <br />
-      <Button color='black' variant="contained" onClick={() => {dispatch(addToCart(lineItem, order))}}>Add to Cart</Button>
+      <Grid container spacing={0} justifyContent="center" alignItems="center">
+        <Grid item xs={7} sx={{}}>
+          <img src={product.imageUrl} style={{ width: '80%', marginLeft: 'auto', marginRight: 'auto', display: 'block'}} /><br />
+        </Grid>
+        <Grid item xs={5} sx={{ paddingRight: '2em'}}>
+          <h3>{product.name}</h3>
+          <h4>{product.brand}</h4>
+          <h2>${sku[0]? sku[0].price : '3em'}</h2>
+          <FormControl sx={{ width: '100%' }}>
+            <InputLabel id="demo-simple-select-label">Size</InputLabel>
+            <Select
+              labelId="demo-simple-select-label"
+              id="demo-simple-select"
+              value={size}
+              label="Size"
+              onChange={(e) => handleSize(e)}
+            >
+              {!color ? <MenuItem value={''}>Please Select a Color</MenuItem> :
+                color.productSKUs.map(sku => sku.availableStock === 0 ?
+                  <MenuItem key={sku.id} value={sku} disabled={true} >{sku.size}  Out of Stock, check back soon!</MenuItem>
+                  : <MenuItem key={sku.id} value={sku}>{sku.size}  {sku.availableStock <= 3 ? `Only ${sku.availableStock} left!` : `${sku.availableStock}`}</MenuItem>)
+              }
+            </Select>
+          </FormControl>
+          <br />
+          <FormControl sx={{ width: '100%' }}>
+            <InputLabel id="demo-simple-select-label">Color</InputLabel>
+            <Select
+              labelId="demo-simple-select-label"
+              id="demo-simple-select"
+              value={color}
+              label="Color"
+              onChange={handleColor}
+              defaultValue={0}
+            >
+              {
+                colors.map(color => <MenuItem key={color.id} value={color}>{color.color}</MenuItem>)
+              }
+            </Select>
+          </FormControl>
+          <br />
+          <Button color='black' variant="contained" onClick={() => { dispatch(addToCart(lineItem, order)) }}>Add to Cart</Button>
+        </Grid>
+        <Grid item xs={12} sx={{padding: '0 3em'}}>
+        <h3>Product Details</h3>
+        <hr />
+        <div>
+            {product.description}  
+        </div>
+        </Grid>
+      </Grid>
     </div>
   )
 }
