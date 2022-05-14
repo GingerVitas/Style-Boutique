9723b7ec42013dc11ac5b5c8ca0325d7bdb4a658
H. Abbasi <72363836+abbasi08@users.noreply.github.com>
2022-05-14 10:45:56 -0400

    password formatting

--- client/components/Password.js
+++ client/components/Password.js
@@ -1,10 +1,10 @@
 import React, { useState } from "react";
-//import { useSelector, useDispatch } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import { FormControl, IconButton, InputAdornment, InputLabel, OutlinedInput, TextField, Visibility, VisibilityOff } from "@mui/material";
 
 const Password = () => {
-  //   const auth = useSelector((state) => state.auth);
-  //const dispatch = useDispatch();
+  const dispatch = useDispatch();
+  const auth = useSelector((state) => state.auth);
 
   const [values, setValues] = useState({
     amount: "",
@@ -14,24 +14,33 @@ const Password = () => {
     showPassword: false
   });
 
+  const handleChangePassword = (ev) => {
+    //this.setState({ [ev.target.name]: ev.target.values.password });
+  };
+
   return (
     <div>
-      <FormControl sx={{ m: 1, width: "25ch" }} variant="outlined">
-        <InputLabel htmlFor="outlined-adornment-password">Password</InputLabel>
-        {/* <OutlinedInput
+      <FormControl sx={{ m: 0, width: "50%" }} variant="outlined">
+        <InputLabel htmlFor="outlined-adornment-password">*******</InputLabel>
+        <OutlinedInput
           id="outlined-adornment-password"
           type={values.showPassword ? "text" : "password"}
           value={values.password}
-          onChange={handleChange("password")}
+          onChange={handleChangePassword("password")}
           endAdornment={
             <InputAdornment position="end">
-              <IconButton aria-label="toggle password visibility" onClick={handleClickShowPassword} onMouseDown={handleMouseDownPassword} edge="end">
-                {values.showPassword ? <VisibilityOff /> : <Visibility />}
+              <IconButton
+                aria-label="toggle password visibility"
+                //   onClick={handleClickShowPassword} onMouseDown={handleMouseDownPassword}
+                edge="end"
+              >
+                {/* {values.showPassword}
+                    ? <VisibilityOff /> : <Visibility />} */}
               </IconButton>
             </InputAdornment>
           }
           label="Password"
-        /> */}
+        />
       </FormControl>
     </div>
   );
